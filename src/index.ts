112385fc33e8ea12ed53cf689e9be559e0d75b23
Chris Wilkinson <c.wilkinson@elifesciences.org>
2023-11-30 15:43:11 +0000

    Log HTTP requests
    Refs https://discordapp.com/channels/795981131316985866/1179003112107810837/1179147870985793576

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,34 @@
-import { HttpClient } from '@effect/platform'
-import { Effect, Layer } from 'effect'
+import { HttpClient } from '@effect/platform-node'
+import { Effect, Layer, LogLevel, Logger } from 'effect'
 import IoRedis from 'ioredis'
 import { program } from './Program.js'
 import * as Redis from './Redis.js'
+
+const HttpClientLive = Layer.succeed(
+  HttpClient.client.Client,
+  HttpClient.client.makeDefault(request =>
+    Effect.Do.pipe(
+      Effect.tap(() => Effect.logDebug('Sending HTTP Request').pipe(Effect.annotateLogs({ headers: request.headers }))),
+      Effect.zipRight(HttpClient.client.fetch()(request)),
+      Effect.tap(response =>
+        Effect.logDebug('Received HTTP response').pipe(
+          Effect.annotateLogs({ status: response.status, headers: response.headers }),
+        ),
+      ),
+      Effect.tapErrorTag('RequestError', error =>
+        Effect.logError('Error sending HTTP request').pipe(
+          Effect.annotateLogs({ reason: error.reason, error: error.error }),
+        ),
+      ),
+      Effect.annotateLogs({
+        url: request.url,
+        urlParams: HttpClient.urlParams.toString(request.urlParams),
+        method: request.method,
+      }),
+      Effect.withLogSpan('fetch'),
+    ),
+  ),
+)
 
 const RedisLive = Layer.effect(
   Redis.Redis,
@@ -15,8 +41,8 @@ const RedisLive = Layer.effect(
   }),
 )
 
-const ProgramLive = Layer.mergeAll(HttpClient.client.layer, RedisLive)
+const ProgramLive = Layer.mergeAll(HttpClientLive, RedisLive)
 
-const runnable = Effect.provide(program, ProgramLive).pipe(Effect.scoped)
+const runnable = Effect.provide(program, ProgramLive).pipe(Effect.scoped, Logger.withMinimumLogLevel(LogLevel.Debug))
 
 await Effect.runPromise(runnable)
