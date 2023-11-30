00e94b39d4e0b3504abaa5d342998f9c5ab32091
Chris Wilkinson <c.wilkinson@elifesciences.org>
2023-11-30 15:51:39 +0000

    Fetch peer review activity from ORCID

---
+++ src/Orcid.ts
@@ -0,0 +1,69 @@
+import { HttpClient } from '@effect/platform'
+import { type ParseResult, Schema } from '@effect/schema'
+import { Context, Effect } from 'effect'
+import { DoiSchema } from './Doi.js'
+import type { OrcidId } from './OrcidId.js'
+
+export interface OrcidAccessToken {
+  readonly token: string
+}
+
+export const OrcidAccessToken = Context.Tag<OrcidAccessToken>()
+
+type PeerReviews = Schema.Schema.To<typeof PeerReviewsSchema>['group']
+
+type GetPeerReviewsForOrcidIdError = HttpClient.error.HttpClientError | ParseResult.ParseError
+
+export const getPeerReviewsForOrcidId = (
+  id: OrcidId,
+): Effect.Effect<HttpClient.client.Client.Default | OrcidAccessToken, GetPeerReviewsForOrcidIdError, PeerReviews> =>
+  Effect.gen(function* (_) {
+    const client = yield* _(orcidClient)
+
+    const response = yield* _(
+      HttpClient.request.get(`${id}/peer-reviews`),
+      client,
+      Effect.flatMap(HttpClient.response.schemaBodyJson(PeerReviewsSchema)),
+    )
+
+    return response.group
+  })
+
+const PeerReviewsSchema = Schema.struct({
+  group: Schema.array(
+    Schema.struct({
+      'external-ids': Schema.struct({
+        'external-id': Schema.tuple(
+          Schema.struct({
+            'external-id-type': Schema.literal('peer-review'),
+            'external-id-value': Schema.string,
+          }),
+        ),
+      }),
+      'peer-review-group': Schema.array(
+        Schema.struct({
+          'external-ids': Schema.struct({
+            'external-id': Schema.tuple(
+              Schema.struct({
+                'external-id-type': Schema.literal('doi'),
+                'external-id-value': DoiSchema,
+              }),
+            ),
+          }),
+        }),
+      ),
+    }),
+  ),
+})
+
+const orcidClient = Effect.gen(function* (_) {
+  const httpClient = yield* _(HttpClient.client.Client)
+  const { token } = yield* _(OrcidAccessToken)
+
+  return httpClient.pipe(
+    HttpClient.client.filterStatusOk,
+    HttpClient.client.mapRequest(HttpClient.request.accept('application/vnd.orcid+json')),
+    HttpClient.client.mapRequest(HttpClient.request.prependUrl('https://api.sandbox.orcid.org/v3.0/')),
+    HttpClient.client.mapRequest(HttpClient.request.bearerToken(token)),
+  )
+})
--- src/Program.ts
+++ src/Program.ts
@@ -1,19 +1,56 @@
-import { Effect, Stream } from 'effect'
+import { Effect, ReadonlyArray, Stream, flow } from 'effect'
+import * as Orcid from './Orcid.js'
 import type * as OrcidId from './OrcidId.js'
 import * as Users from './Users.js'
 import * as Zenodo from './Zenodo.js'
 
-const processUser = ({ orcidId }: { orcidId: OrcidId.OrcidId; accessToken: string }) =>
+const getPeerReviewsDoisForOrcidId = flow(
+  Orcid.getPeerReviewsForOrcidId,
+  Effect.flatMap(
+    ReadonlyArray.findFirst(
+      group => group['external-ids']['external-id'][0]['external-id-value'] === 'orcid-generated:prereview',
+    ),
+  ),
+  Effect.map(group =>
+    ReadonlyArray.map(
+      group['peer-review-group'],
+      peerReview => peerReview['external-ids']['external-id'][0]['external-id-value'],
+    ),
+  ),
+  Effect.catchTag('NoSuchElementException', () => Effect.succeed([])),
+  Effect.tap(reviews =>
+    Effect.gen(function* (_) {
+      if (reviews.length < 1) {
+        return yield* _(Effect.logInfo('No reviews on ORCID found'))
+      }
+      yield* _(Effect.logInfo('Found reviews on ORCID').pipe(Effect.annotateLogs('total', reviews.length)))
+    }),
+  ),
+)
+
+const getPeerReviewsOnZenodoForOrcidId = flow(
+  Zenodo.getReviewsByOrcidId,
+  Effect.tap(reviews =>
+    Effect.gen(function* (_) {
+      if (reviews.total < 1) {
+        return yield* _(Effect.logInfo('No reviews on Zenodo found'))
+      }
+      yield* _(Effect.logInfo('Found reviews on Zenodo').pipe(Effect.annotateLogs('total', reviews.total)))
+    }),
+  ),
+)
+const processUser = ({ orcidId, accessToken }: { orcidId: OrcidId.OrcidId; accessToken: string }) =>
   Effect.gen(function* (_) {
     yield* _(Effect.logInfo('Processing user'))
 
-    const reviews = yield* _(Zenodo.getReviewsByOrcidId(orcidId))
-
-    if (reviews.total < 1) {
-      return yield* _(Effect.logInfo('No reviews found'))
-    }
-
-    yield* _(Effect.logInfo('Found reviews').pipe(Effect.annotateLogs('total', reviews.total)))
-  }).pipe(Effect.annotateLogs('orcidId', orcidId))
+    yield* _(
+      Effect.all([getPeerReviewsOnZenodoForOrcidId(orcidId), getPeerReviewsDoisForOrcidId(orcidId)], {
+        concurrency: 'inherit',
+      }),
+    )
+  }).pipe(
+    Effect.annotateLogs('orcidId', orcidId),
+    Effect.provideService(Orcid.OrcidAccessToken, { token: accessToken }),
+  )
 
 export const program = Users.getUsers.pipe(Stream.runForEach(processUser)).pipe(Effect.scoped)
