08ce40324f92087780606cb41750ca257bdb798a
Chris Wilkinson <c.wilkinson@elifesciences.org>
2023-12-06 14:42:38 +0000

    Reduce size of dependencies

--- src/Doi.ts
+++ src/Doi.ts
@@ -1,9 +1,12 @@
 import { Schema } from '@effect/schema'
-import doi from 'doi-ts'
-import { Brand } from 'effect'
+import doiRegex from 'doi-regex'
+import { Brand, type Predicate } from 'effect'
 
 export type Doi = Brand.Branded<string, 'Doi'>
 
-export const Doi = Brand.refined<Doi>(doi.isDoi, s => Brand.error(`Expected ${s} to be a DOI`))
+const isDoi: Predicate.Refinement<unknown, Doi> = (u): u is Doi =>
+  typeof u === 'string' && doiRegex({ exact: true }).test(u) && !u.endsWith('/.') && !u.endsWith('/..')
+
+export const Doi = Brand.refined<Doi>(isDoi, s => Brand.error(`Expected ${s} to be a DOI`))
 
 export const DoiSchema = Schema.string.pipe(Schema.fromBrand(Doi))
--- src/OrcidId.ts
+++ src/OrcidId.ts
@@ -1,8 +1,16 @@
 import { Schema } from '@effect/schema'
-import { Brand } from 'effect'
-import { isOrcid } from 'orcid-id-ts'
+import { Brand, type Predicate } from 'effect'
+import orcidUtils from 'orcid-utils'
 
 export type OrcidId = Brand.Branded<string, 'OrcidId'>
+
+const isOrcid: Predicate.Refinement<unknown, OrcidId> = (u): u is OrcidId => {
+  try {
+    return typeof u === 'string' && orcidUtils.toDashFormat(u) === u
+  } catch {
+    return false
+  }
+}
 
 export const OrcidId = Brand.refined<OrcidId>(isOrcid, s => Brand.error(`Expected ${s} to be an ORCID iD`))
 
