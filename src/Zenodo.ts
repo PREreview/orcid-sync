56da9191fc89e6db593937f3ca0394b9295b56fc
Chris Wilkinson <c.wilkinson@elifesciences.org>
2024-04-19 09:40:47 +0100

    Match changes

--- src/Zenodo.ts
+++ src/Zenodo.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@effect/platform'
 import { type ParseResult, Schema } from '@effect/schema'
-import { Context, Effect, type ReadonlyRecord } from 'effect'
+import { Context, Effect, type Record } from 'effect'
 import { DoiSchema } from './Doi.js'
 import type * as OrcidId from './OrcidId.js'
 import * as Temporal from './Temporal.js'
@@ -27,7 +27,7 @@ export const getReviewsByOrcidId = (orcid: OrcidId.OrcidId) =>
   })
 
 const getRecords = (
-  params: ReadonlyRecord.ReadonlyRecord<string, string>,
+  params: Record.ReadonlyRecord<string, string>,
 ): Effect.Effect<Records, GetRecordsForOrcidIdError, ZenodoConfig | HttpClient.client.Client.Default> =>
   Effect.gen(function* (_) {
     const client = yield* _(zenodoClient)
